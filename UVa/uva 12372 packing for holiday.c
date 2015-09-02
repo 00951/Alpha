3af9776f41cf8ebc5f793977c69ffecdf9d498c6
GM Arif <gmarif.online@gmail.com>
2015-09-02 15:05:47 +0600

    Create uva 12372 packing for holiday.c

---
+++ UVa/uva 12372 packing for holiday.c
@@ -0,0 +1,24 @@
+/*
+* GM Arif
+* CSE, Jhangirnagar University
+* id: gmarif_online (uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,T,cs=0;
+    scanf("%d", &T);
+
+    while(T--){
+        while(scanf("%d %d %d", &a, &b, &c)==3){
+        cs++;
+        if(a<=20 && b<=20 && c<=20)
+            printf("Case %d: good\n");
+        else
+            printf("Case %d: bad\n");
+    }
+}
+    return 0;
+}
