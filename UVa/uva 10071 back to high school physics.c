3597b207fcbdbce4ec8b8c9d7831bbb2d53143da
GM Arif <gmarif.online@gmail.com>
2015-09-02 18:09:26 +0600

    Create uva 10071 back to high school physics.c

---
+++ UVa/uva 10071 back to high school physics.c
@@ -0,0 +1,17 @@
+/*
+* GM Arif
+* CSE, Jhangirnagar University
+* id: gmarif_online (uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int v,t,s;
+    while(scanf("%d %d", &v, &t)==2){
+        printf("%d\n", v*2*t);
+    }
+    return 0;
+}
