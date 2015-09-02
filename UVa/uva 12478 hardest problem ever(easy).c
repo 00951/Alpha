890dbc3ea046fa77ec48d190f9980913d71f6ace
GM Arif <gmarif.online@gmail.com>
2015-09-03 01:49:15 +0600

    Create uva 12478 hardest problem ever(easy).c

---
+++ UVa/uva 12478 hardest problem ever(easy).c
@@ -0,0 +1,14 @@
+/*
+* GM Arif
+* CSE, Jhangirnagar University
+* id: gmarif_online (uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("KABIR\n");
+    return 0;
+}
