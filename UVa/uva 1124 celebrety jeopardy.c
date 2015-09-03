430db245c23d1d03d39f80f629f7cf8331967913
GM Arif <gmarif.online@gmail.com>
2015-09-03 21:09:35 +0600

    Create uva 1124 celebrety jeopardy.c

---
+++ UVa/uva 1124 celebrety jeopardy.c
@@ -0,0 +1,18 @@
+/*
+* GM Arif
+* CSE, Jhangirnagar University
+* id: gmarif_online (uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int str[10000];
+
+    while(gets(str)){
+        puts(str);
+    }
+    return 0;
+}
