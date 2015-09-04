b635f028e71cd8da29a5c30fe74fa863b9a1af92
GM Arif <gmarif.online@gmail.com>
2015-09-04 20:08:38 +0600

    Create uva 12646 zero or one.c

---
+++ UVa/uva 12646 zero or one.c
@@ -0,0 +1,30 @@
+/*
+* GM Arif
+* CSE, Jhangirnagar University
+* id: gmarif_online (uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    while(scanf("%d %d %d",&a, &b, &c)==3){
+        if(a==0 && (b==1 && c==1))
+            printf("A\n");
+        else if(a==1 && (b==0 && c==0))
+            printf("A\n");
+        else if(b==0 && (c==1 && a==1))
+            printf("B\n");
+        else if(b==1 && (c==0 && a==0))
+            printf("B\n");
+        else if(c==0 && (a==1 && b==1))
+            printf("C\n");
+        else if(c==1 && (a==0 && b==0))
+            printf("C\n");
+        else
+            printf("*\n");
+    }
+    return 0;
+}
