4c2e03081b552511ba748ff5c504e43464ddb5ae
GM Arif <gmarif.online@gmail.com>
2015-10-10 23:24:29 +0600

    Update *uva 10696 f91.c

--- UVa/*uva 10696 f91.c
+++ UVa/*uva 10696 f91.c
@@ -1,8 +1,42 @@
+
 /*
 * GM Arif
 * CSE, Jhangirnagar University
 * id: gmarif_online (uva/codeforces/codechef/hackerrank/spoj/topcoder)
 */
+
+
+#include <stdio.h>
+
+int n;
+
+/*
+int f91(int n){
+    if(n<=100)
+        return f91(f91(n+11));
+    else
+        return n-10;
+}
+*/
+
+int f91(int n){
+    return (n <= 100) ? f91(f91(n + 11)) : n-10;
+}
+
+int main()
+{
+    while(scanf("%d", &n)==1){
+        if(n==0)
+            break;
+        printf("f91(%d) = %d\n", n, f91(n));
+    }
+    return 0;
+}
+
+
+
+
+// :/ --- OR --- 
 
 
 #include <stdio.h>
