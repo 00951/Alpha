d8b330474255b4b1879d6022dea093566596662e
GM Arif <gmarif.online@gmail.com>
2015-10-14 04:24:06 +0600

    Update UVa 10394 Twin Primes.c

--- UVa/UVa 10394 Twin Primes.c
+++ UVa/UVa 10394 Twin Primes.c
@@ -34,3 +34,57 @@ int main(){
         }
     return 0;
 }
+
+
+
+
+
+
+// ..............................
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 20000007
+
+int arr[MAX], store[MAX], twin[MAX];
+
+
+int main(){
+
+    int i,j,k,x,l, count;
+
+        memset(store, 9, sizeof store);
+
+        k=1,l=1;
+
+        for(i=2; i<=MAX; i++){
+
+            if(arr[i]==0){
+                for(j=2*i; j<=MAX; j+=i){
+                    arr[j]=1;
+                }
+            }
+
+            if(arr[i]==0){
+                store[k]=i; //array of primes
+                if(store[k]-store[k-1]==2){
+                    twin[l++]=store[k-1];   //array of twin primes
+                }
+                k++;
+            }
+
+        }
+
+
+        while(scanf("%d", &x)==1){
+            printf("(%d, %d)\n", twin[x], twin[x]+2);
+        }
+
+    return 0;
+}
