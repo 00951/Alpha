1eaee1e41ea5532bfb01d505d9a833f3b785b0e2
GM Arif <gmarif.online@gmail.com>
2015-10-24 20:47:26 +0600

    Create ***Template.cpp

---
+++ UVa/***Template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long            ll
+typedef long long unsigned   llu
+typedef double               dl
+
+//~ Define
+#define pi acos(-1.0)
+#define zero(a) memset(a, 0, sizeof a)
+#define minus(a) memset(-1, 0, sizeof a)
+#define gcd(a,b) __gcd(a,b)
+
+//~ Input
+#define sci(a) scanf("%d", &a);
+#define sci(a,b) scanf("%d%d", &a, &b);
+#define sci(a,b,c) scanf("%d%d%d", &a, &b, &c);
+#define scl(a) scanf("%lld", &a);
+#define scll(a,b) scanf("%lld%lld", &a, &b);
+#define sclll(a,b,c) scanf("%lld%lld%lld", &a, &b, &c);
+#define scd(a) scanf("%lf", &a);
+
+int main()
+{
+    
+    return 0;
+}
