e4b53611512490f63296bfa1038957ce717b4db0
GM Arif <gmarif.online@gmail.com>
2015-10-25 03:22:42 +0600

    Create Sieve of Eratosthenes.cpp

---
+++ Algorithms/Primary-Algos/Sieve of Eratosthenes.cpp
@@ -0,0 +1,44 @@
+/*
+* GM Arif
+* CSE, Jahangirnagar University
+* id: gmarif_online(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 20000007
+
+int arr[MAX];
+int n,i,j;
+
+//********************************//
+
+void sieve()
+{
+    for(i=2; i<=MAX; i++){
+        if(arr[i]==0){
+            for(j=2*i; j<=MAX; j+=i){
+                arr[j]=1;
+            }
+        }
+    }
+}
+
+//********************************//
+
+int main()
+{
+    sieve();
+
+    while(scanf("%d", &n)==1){
+        
+        if(arr[n]==0)
+            printf("Prime\n");
+        else if(arr[n]==1)
+            printf("Not Prime\n");
+        
+    }
+    return 0;
+}
