94cea453d047902ab3e33bdb71653a9a5e6f2b9a
GM Arif <gmarif.online@gmail.com>
2015-10-25 17:28:06 +0600

    Create lightoj 1107 how cow.c

---
+++ LightOJ/lightoj 1107 how cow.c
@@ -0,0 +1,30 @@
+/*
+* GM Arif
+* CSE, Jahangirnagar University
+* id: gmarif_online(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    long long numcow, T, x, y, x1, y1, x2, y2, cs=0;
+    scanf("%lld", &T);
+    while(T--){
+        scanf("%lld %lld %lld %lld", &x1, &y1, &x2, &y2);
+        scanf("%lld", &numcow);
+        cs++;
+        printf("Case %lld:\n", cs);
+
+        while(numcow--){
+            scanf("%lld %lld", &x, &y);
+
+            if((x>x1 && x<x2)&&(y>y1 && y<y2))
+                printf("Yes\n");
+            else
+                printf("No\n");
+        }
+    }
+    return 0;
+}
