a55158517872034db9394539a85b3ba597a2e900
GM Arif <gmarif.online@gmail.com>
2015-10-26 05:04:08 +0600

    Update xtra notes.cpp

--- Algorithms/General-Topic-Notes/xtra notes.cpp
+++ Algorithms/General-Topic-Notes/xtra notes.cpp
@@ -3,4 +3,25 @@
 numdigit= (n>0) ? (int) log10 ((double) n) + 1 : 1;
 
 
+
+*** A Recursive Function to return 10 for input 1, to return 100 for input 2 & to return 1000 for 3
+
+long long x100for2_1000for3_func(int x)
+{
+    if(x==0)
+        return 1;
+    else
+        return 10*x100for2_1000for3_func(x-1);
+}
+
+int main()
+{
+    int n,i,j,k;
+    cin>>n;
+    cout<<x100for2_1000for3_func(n)<<endl;
+    main();
+}
+
+
+
 *** 
