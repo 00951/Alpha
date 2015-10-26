cbee591f2b84d9e1abb3c7f9d8d204837cebdcdb
GM Arif <gmarif.online@gmail.com>
2015-10-27 04:35:58 +0600

    Create 513A - Game.cpp

---
+++ CodeForces/513A - Game.cpp
@@ -0,0 +1,26 @@
+/*
+* GM Arif
+* CSE, Jahangirnagar University
+* id: gmarif_online(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+Problem: http://codeforces.com/problemset/problem/513/A
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,k1,k2;
+
+    while(scanf("%d%d%d%d", &n1,&n2,&k1,&k2)==4){
+        if(n1>n2)
+            printf("First\n");
+        else if(n1<n2)
+            printf("Second\n");
+        else
+            printf("Second\n");
+    }
+
+    return 0;
+}
