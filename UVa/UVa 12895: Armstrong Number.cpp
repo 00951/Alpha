938c5a4ffb55ec3fa6816d47ae8c09a5ae673f5b
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-11-18 22:31:48 +0600

    Create UVa 12895: Armstrong Number.cpp

---
+++ UVa/UVa 12895: Armstrong Number.cpp
@@ -0,0 +1,58 @@
+/****
+* GM Arif Shuvo
+* CSE, Jahangirnagar University
+* id: aarif_shuvo(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+****/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define MAX 12345
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+
+ll order(ll n){
+    ll c=0;
+    while(n>0){
+        n=n/10;
+        c++;
+    }
+    return c;
+}
+
+
+ll pown(ll p, ll n){
+    ll sum=1;
+    rep(i,1,n){
+        sum*=p;
+    }
+    return sum;
+}
+
+int main()
+{
+    ll T,n;
+    cin>>T;
+
+    while(T--){
+        cin >> n;
+        ll p=order(n);
+        ll tmp=n;
+        ll add=0;
+
+        while(tmp>0){
+            add+=pown(tmp%10, p);
+            tmp/=10;
+        }
+
+        if(add==n)
+            cout << "Armstrong" << endl;
+        else
+            cout << "Not Armstrong" << endl;
+    }
+
+    return 0;
+}
+
