694f4d2c4e04eece7ea8b35597553619eec26cd7
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-11-24 23:23:34 +0600

    Update Sherlock And Array.cpp

--- HackerRank/2015/Sherlock And Array.cpp
+++ HackerRank/2015/Sherlock And Array.cpp
@@ -1,1 +1,40 @@
-hi..
+/*
+* GM Arif Shuvo
+* CSE, Jahangirnagar University
+* id: aarif_shuvo(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[123456], sum[123456], t, n, sl, sr, flg;
+    cin >> t;
+    while(t--)
+    {
+        flg=0;
+        cin >> n;
+        sum[0]=0;
+        for(int i=1; i<=n; i++)
+        {
+            cin >> arr[i];
+            sum[i]=sum[i-1]+arr[i];
+        }
+                
+        for(int i=1; i<=n; i++)
+        {
+            sl = sum[i-1] - sum[0]; //sum of 0 to i-1 -> sum[r] - sum[l-1]
+            sr = sum[n] - sum[i]; // sum of i+1 to n
+                                    // cout <<sl << " " << sr << endl;
+            if(sl==sr)
+            {
+                flg=1;
+                break;
+            }
+        }
+        if(flg==1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
