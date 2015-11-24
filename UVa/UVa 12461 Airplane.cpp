174a7645b4cd46ed6d5be52b9f9da96bb856fdab
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-11-24 23:19:54 +0600

    Create UVa 12461 Airplane.cpp

---
+++ UVa/UVa 12461 Airplane.cpp
@@ -0,0 +1,19 @@
+/*
+* GM Arif Shuvo
+* CSE, Jahangirnagar University
+* id: aarif_shuvo(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    while(cin>>n){
+        if(n==0) break;
+        cout<<1<<"/"<<2<<endl;
+    }
+    return 0;
+}
