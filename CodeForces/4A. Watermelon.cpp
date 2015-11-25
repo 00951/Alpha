c479ce22ceab08be840c77135359cee5765f739f
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-11-26 02:02:08 +0600

    Update and rename 4A - Watermelon.cpp to 4A. Watermelon.cpp

--- CodeForces/4A. Watermelon.cpp
+++ CodeForces/4A. Watermelon.cpp
@@ -1,7 +1,7 @@
 /*
-* GM Arif
+* GM Arif Shuvo
 * CSE, Jahangirnagar University
-* id: gmarif_online(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+* id: aarif_shuvo(uva/codeforces/codechef/hackerrank/spoj/topcoder)
 */
 
 
@@ -24,7 +24,6 @@ int main()
             else
                 printf("NO\n");
         }
-
     }
     return 0;
 }
