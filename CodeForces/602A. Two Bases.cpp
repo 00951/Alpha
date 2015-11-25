6b9b2be75cf6d8a69c899e9e2c1d802f6020d0ed
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-11-25 18:32:32 +0600

    Create 602A. Two Bases.cpp

---
+++ CodeForces/602A. Two Bases.cpp
@@ -0,0 +1,63 @@
+/*
+* GM Arif Shuvo
+* CSE, Jahangirnagar University
+* id: aarif_shuvo(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+
+http://codeforces.com/contest/602/problem/A
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+#define MAX 12345
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+
+LL pown(LL b, LL p){
+    LL ans=1;
+    rep(i,1,p){
+        ans*=b;
+    }
+    return ans;
+}
+
+int main()
+{
+
+    LL dig,bs;
+    LL x[MAX], y[MAX];
+
+    cin>>dig>>bs;
+        LL i=0;
+    while(dig--){
+        cin>>x[i++];
+    }
+    LL it=0, xsum=0;
+    rev(k,i-1,0){
+        xsum+=x[k]*pown(bs,it++);
+    }
+
+    cin >> dig >> bs;
+    i=0;
+    while(dig--){
+        cin>>y[i++];
+    }
+
+    it=0; LL ysum=0;
+    rev(k,i-1,0){
+        ysum+=y[k]*pown(bs,it++);
+    }
+
+    if(xsum==ysum)
+        printf("=\n");
+    else if(xsum>ysum)
+        printf(">\n");
+    else
+        printf("<\n");
+
+    return 0;
+}
