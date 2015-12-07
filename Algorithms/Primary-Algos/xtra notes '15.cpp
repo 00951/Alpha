293dcde2cb4f690611e14632410a7de998adef72
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-12-08 00:30:20 +0600

    Create xtra notes '15.cpp

---
+++ Algorithms/Primary-Algos/xtra notes '15.cpp
@@ -0,0 +1,53 @@
+***// Find the Number of digits in any positive number
+
+numdigit= (n>0) ? (int) log10 ((double) n) + 1 : 1;
+
+
+***// Equality check for pow function
+
+if(ceil(pow(5,2))==25)
+    cout << "Same" << endl;
+
+
+***// A Recursive Function to return 10 for input 1, to return 100 for input 2 & to return 1000 for 3
+
+
+long long x100for2_1000for3_func(int x)
+{
+    if(x==0)
+        return 1;
+    else
+        return 10*x100for2_1000for3_func(x-1);
+}
+
+int main()
+{
+    int n,i,j,k;
+    cin>>n;
+    cout<<x100for2_1000for3_func(n)<<endl;
+    main();
+}
+
+
+
+***// Find Leap Year or Not
+
+const string L = "Leap Year";
+
+ if( ( yr%4==0 && yr%100!=0 ) || yr%400==0 ) 
+            
+            cout<< L <<endl;
+    
+
+
+***// Modulus of a string number 
+
+ll mod(string num, ll m){
+
+    ll mod21=0, len=num.size();
+
+    for(ll i=0; i<len; i++){
+        mod21=((mod21*10) + num[i]-'0') % m;
+    }
+    return mod21;
+}
