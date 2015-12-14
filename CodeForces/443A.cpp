ff5e71c6ee7f89f6cabf4d68695cd311beaf4ef2
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-12-14 12:29:43 -0800

    Create 443A.cpp

---
+++ CodeForces/443A.cpp
@@ -0,0 +1,47 @@
+/*
+* GM Arif Shuvo
+* CSE, Jahangirnagar University(44)
+* id: aarif_shuvo(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define LL  long long
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define prntcs       cout<<"Case "<<++cs<<":";
+#define prnt1(a)     cout<<a<<endl
+#define prnt2(a,b)   cout<<a<<" "<<b<<endl
+#define prnt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<endl
+#define debug_array(a,n) for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debug_matrix(mat,row,col) for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+template<class T1> void debug(T1 e){cout<<e<<endl;}
+template<class T1,class T2> void debug(T1 e1,T2 e2){cout<<e1<<"\t"<<e2<<endl;}
+template<class T1,class T2,class T3> void debug(T1 e1,T2 e2,T3 e3){cout<<e1<<"\t"<<e2<<"\t"<<e3<<endl;}
+template<class T1,class T2,class T3,class T4> void debug(T1 e1,T2 e2,T3 e3,T4 e4){cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<endl;}
+template<class T1,class T2,class T3,class T4,class T5> void debug(T1 e1,T2 e2,T3 e3,T4 e4,T5 e5){cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<"\t"<<e5<<endl;}
+
+#define MX 12345
+
+
+int main()
+{
+    char s[MX];
+    LL a[MX];
+    memset(a,0,sizeof a);
+    gets(s);
+    LL l=strlen(s);
+    rep(i,0,l-1){
+        if(isalpha(s[i]))
+            a[s[i]]++;
+    }
+    LL c=0;
+    rep(i,97,122){
+        if(a[i]>0)
+            c++;
+    }
+    prnt1(c);
+    return 0;
+}
+
+
