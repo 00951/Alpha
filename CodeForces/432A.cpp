e738f29e727682caebcd73b281fe2ccd1b075a57
GM Arif Shuvo <aarif.shuvo@gmail.com>
2015-12-15 01:15:47 -0800

    Create 432A.cpp

---
+++ CodeForces/432A.cpp
@@ -0,0 +1,43 @@
+/*
+* GM Arif Shuvo
+* CSE, Jahangirnagar University
+* id: aarif_shuvo(uva/codeforces/codechef/hackerrank/spoj/topcoder)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define LL  long long
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define prntcs       printf("Case %I64d: ", ++cs);
+#define prnt1(a)     cout<<a<<endl
+#define prnt2(a,b)   cout<<a<<" "<<b<<endl
+#define prnt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<endl
+#define debug_array(a,n) for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debug_matrix(mat,row,col) for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+template<class T1> void debug(T1 e){cout<<e<<endl;}
+template<class T1,class T2> void debug(T1 e1,T2 e2){cout<<e1<<"\t"<<e2<<endl;}
+template<class T1,class T2,class T3> void debug(T1 e1,T2 e2,T3 e3){cout<<e1<<"\t"<<e2<<"\t"<<e3<<endl;}
+template<class T1,class T2,class T3,class T4> void debug(T1 e1,T2 e2,T3 e3,T4 e4){cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<endl;}
+template<class T1,class T2,class T3,class T4,class T5> void debug(T1 e1,T2 e2,T3 e3,T4 e4,T5 e5){cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<"\t"<<e5<<endl;}
+
+#define MX 12345
+
+int main()
+{
+    LL n,k,a[MX];
+    cin>>n>>k;
+    rep(i,1,n)
+        cin>>a[i];
+    sort(a+1, a+n+1);
+    LL j=3,c=0;
+    while(j<=n){
+        if(5-a[j]>=k)
+            c++;
+        j+=3;
+    }
+    prnt1(c);
+    return 0;
+}
+
+
