d3dfebc76540c59eb3b7c3a755b5edfeb9d1680b
GM Ariful Islam <aarif.shuvo@gmail.com>
2015-12-28 17:11:02 -0800

    Create 44A.cpp

---
+++ CodeForces/2015/44A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define LL  long long
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define csprnt       cout<<"Case "<<++cs<<": ";
+#define prnt1(a)     cout<<a<<endl
+#define prnt2(a,b)   cout<<a<<" "<<b<<endl
+#define prnt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<endl
+
+/*--BUG-ER-HAT--*/
+#define debug_array(a,n) for(int i=0;i<n;i++) cerr<<a[i]<<" "; cerr<<endl;
+#define debug_matrix(mat,row,col) for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cerr<<mat[i][j]<<" ";cerr<<endl;}
+#define READ(filename) freopen(filename, "r", stdin);
+#define WRITE(filename) freopen(filename, "w", stdout);
+#define hi printf("Hello World!\n")
+#define ndl printf("\n")
+
+template<class T1> void debug(T1 e){cout<<e<<endl;}
+template<class T1,class T2> void debug(T1 e1,T2 e2){cout<<e1<<"\t"<<e2<<endl;}
+template<class T1,class T2,class T3> void debug(T1 e1,T2 e2,T3 e3){cout<<e1<<"\t"<<e2<<"\t"<<e3<<endl;}
+template<class T1,class T2,class T3,class T4> void debug(T1 e1,T2 e2,T3 e3,T4 e4){cout<<e1<<"\t"<<e2<<"\t"<<e3<<"\t"<<e4<<endl;}
+
+
+/*--CONSTANTS--*/
+#define MX 12345
+
+
+//^^start plz :')
+
+
+int main()
+{
+    //READ("F:/in.txt");
+
+   LL T,x=1,c=0,f;
+   string sa[MX],s;
+   cin>>T;
+   getchar();
+   getline(cin,s);
+   sa[x]=s;
+   rep(i,1,T-1){
+       f=0;
+        getline(cin,s);
+        rep(j,0,x){
+            if(s.compare(sa[j])==0){
+                f=1;
+                break;
+            }
+        }
+        if(!f) sa[++x]=s;
+   }
+    prnt1(x);
+
+    return 0;
+}
+
+
