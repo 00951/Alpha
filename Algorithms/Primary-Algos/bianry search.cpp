e2e64bbe7594830434c3cc6c1a2bd4efcea82453
GM Ariful Islam <aarif.shuvo@gmail.com>
2015-12-30 12:53:44 -0800

    Update bianry search.cpp

--- Algorithms/Primary-Algos/bianry search.cpp
+++ Algorithms/Primary-Algos/bianry search.cpp
@@ -1,17 +1,58 @@
-#include<stdio.h>
-
-int bsearch(int l,int h,int a[],int c)
+LL bsearch(LL f,LL l,LL a[],LL x)
 {
-    int pos;
-    while(l<=h)
+    LL ps;
+    
+    if(x<a[0] || x>a[l]) return -1;
+    
+    while(f<=l)
     {
-        pos=(l+h)/2;
-        if(a[pos]==c)
-            return pos;
-        else if(a[pos]<c)
-            l=pos+1;
+        LL md=(f+l)/2;
+        if(x==a[md])
+            return md;
+        else if(x>a[md])
+            f=md+1;
         else
-            h=pos-1;
+            l=md-1;
     }
+    return 0;
+}
+
+
+---------------------------------------------------------------
+
+
+#include<stdio.h>
+#define LL long long
+
+LL bsearch(LL f,LL l,LL a[],LL x)
+{
+    LL ps=-1;
+
+    if(x<a[0] || x>a[l]) return -1;
+
+    while(f<=l)
+    {
+        LL md=(f+l)/2;
+        if(x==a[md])
+            return md;
+        else if(x>a[md])
+            f=md+1;
+        else
+            l=md-1;
+    }
+    return 0;
+}
+
+int main()
+{
+    LL n;
+    
+    LL a[5]={0,1,2,3,4};
+    
+    scanf("%lld", &n);
+
+    printf("%lld's index is %lld\n", n, bsearch(0,4,a,n));
+
+    main();
     return 0;
 }
