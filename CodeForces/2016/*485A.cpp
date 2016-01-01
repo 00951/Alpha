b977128c139a8dc410431f9421e7df3e38fa4013
Faltu Coder - Kisu Pare Na :( :( <aarif.shuvo@gmail.com>
2015-12-31 16:17:35 -0800

    Create *485A.cpp

---
+++ CodeForces/2016/*485A.cpp
@@ -0,0 +1,36 @@
+/******************************************************************
+***                                                             ***
+***   Author     : GM Arif Shuvo                                ***
+***   University : CSE, Jahangirnagar University(44)            ***
+***   Handle     : aarif_shuvo (codeforces/topcoder/codechef)   ***
+***                                                             ***
+******************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define LL  long long
+
+#define MX 123456
+
+bool vstd[MX];
+
+int main()
+{
+    LL a,m;
+    cin>>a>>m;
+    while(1){
+        LL rem=a%m;
+        if(!rem){
+            puts("Yes");
+            return 0;
+        }
+        if(vstd[rem]){
+            puts("No");
+            return 0;
+        }
+        vstd[rem]=1;
+        a+=rem;
+    }
+    return 0;
+}
