65e16071704e7ee4861fb4719829e36666bc7c80
Faltu Coder - Kisu Pare Na :( :( <aarif.shuvo@gmail.com>
2016-01-06 03:35:50 -0800

    Update base conversion.cpp

--- Algorithms/Primary-Algos/base conversion.cpp
+++ Algorithms/Primary-Algos/base conversion.cpp
@@ -1,3 +1,38 @@
+/* Function to convert decimal to binary.*/
+
+int decimal_binary(int n)  
+{
+    int rem, i=1, binary=0;
+    while (n!=0)
+    {
+        rem=n%2;
+        n/=2;
+        binary+=rem*i;
+        i*=10;
+    }
+    return binary;
+}
+
+
+
+/* Function to convert binary to decimal.*/
+
+int binary_decimal(int n) 
+
+{
+    int decimal=0, i=0, rem;
+    while (n!=0)
+    {
+        rem = n%10;
+        n/=10;
+        decimal += rem*pow(2,i);
+        ++i;
+    }
+    return decimal;
+}
+
+
+
 //** Decimal to Binary using Recursion
 
 void d2b(LL n)
