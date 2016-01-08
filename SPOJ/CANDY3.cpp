ad4da1ef7e5c87461368c227bd9ef10bf5c74c1a
Faltu Coder - Kisu Pare Na :( :( <aarif.shuvo@gmail.com>
2016-01-08 19:45:36 +0600

    Create CANDY3.cpp

---
+++ SPOJ/CANDY3.cpp
@@ -0,0 +1,52 @@
+/******************************************************************
+***                                                             ***
+***   Author     : GM Arif Shuvo                                ***
+***   University : CSE, Jahangirnagar University                ***
+***   Handle     : aarif_shuvo (codeforces/topcoder/codechef)   ***
+***                                                             ***
+******************************************************************/
+
+#include <bits/stdc++.h>
+#define scl(x) scanf("%lld",&x)
+#define scl2(x,y) scanf("%lld %lld",&x,&y)
+#define scl3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define scl4(w,x,y,z) scanf("%lld %lld %lld %lld",&w,&x,&y,&z)
+#define scl5(x,y,z,p,q) scanf("%lld %lld %lld %lld %lld",&x,&y,&z,&p,&q)
+#define LL  long long
+#define LLU unsigned long long
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define CLR(a)     memset(a,0,sizeof(a))
+#define SET(a)     memset(a,-1,sizeof(a))
+#define prnt1(a)     cout<<a<<endl
+#define prnt2(a,b)   cout<<a<<" "<<b<<endl
+#define prnt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<endl
+#define csprnt       printf("Case %lld: ", ++cs);
+#define READ(filename) freopen(filename, "r", stdin);
+#define WRITE(filename) freopen(filename, "w", stdout);
+#define hi printf("Hello World!\n")
+#define ndl printf("\n")
+using namespace std;
+
+#define MX 12345
+
+int main()
+{
+    //READ("F:/in.txt");
+    //WRITE("F:/out.txt");
+    LL i,j,k,l,m,n,x,sm,T,mod,tmp,cs=0;
+    scl(T);
+    while(T--){
+        getchar();
+        scl(n);
+        sm=0;
+        rep(i,1,n){
+            scl(x);
+            sm += x;
+            sm %= n;
+        }
+        if(sm) puts("NO");
+        else puts("YES");
+    }
+    return 0;
+}
