d9aa9e0bbcaaaac1e3dc4f08b80455011726adf2
Faltu Coder - Kisu Pare Na :( :( <aarif.shuvo@gmail.com>
2016-01-10 19:24:44 +0600

    Update recursion primary codes.cpp

--- Algorithms/Primary-Algos/recursion primary codes.cpp
+++ Algorithms/Primary-Algos/recursion primary codes.cpp
@@ -51,3 +51,26 @@ int main()
 {
     print(1);
 }
+
+
+
+// factorial using recursion
+
+int fact(int n)
+{
+    if(n==1)
+        return 1;
+    return n*fact(n-1);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    cout << fact(n) << endl;
+    return 0 ;
+}
+
+
+
+// 
