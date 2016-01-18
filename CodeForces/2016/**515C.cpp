544ce2899e5849532f7de3073dcb4516e98944f1
Bad Robot Productions <aarif.shuvo@gmail.com>
2016-01-18 15:08:16 +0600

    Update and rename *515C.cpp to **515C.cpp

--- CodeForces/2016/**515C.cpp
+++ CodeForces/2016/**515C.cpp
@@ -10,7 +10,7 @@
 #define ALL(x) (x).begin(),(x).end()
 #define REP(i,n) for(int i=0;i<n;++i)
 #define REPP(i,a,b) for(int i=a;i<b;++i)
-#define REVV(i,b,a) for(int i=b;i>=a;--i)
+#define REV(i,b,a) for(int i=b;i>=a;--i)
 #define PB push_back
 #define RS(x) scanf("%s",(x))
 #define RI(x) scanf("%d",&(x))
@@ -18,7 +18,7 @@
 #define RIII(x,y,z) scanf("%d%d%d",&(x),&(y),&(z))
 #define CLR(a)  memset(a,0,sizeof(a))
 #define SET(a)  memset(a,-1,sizeof(a))
-#define DRI(x) LL (x); scanf("%lld",&x)
+#define DRI(x) LL x; scanf("%lld",&x)
 #define DRII(x,y) LL x,y; scanf("%lld%lld",&x,&y)
 #define DRIII(x,y,z) LL x,y,z; scanf("%lld%lld%lld",&x,&y,&z)
 #define prnt1(a)     cout<<a<<endl
@@ -34,22 +34,25 @@ using namespace std;
 const int MOD = 1e9+7;
 const int MX = 1e5+10;
 
-string con[10]={"", "", "2", "3", "223", "5", "53", "7", "7222", "7332"};   
+string con[10]={"", "", "2", "3", "223", "5", "53", "7", "7222", "7332"};
 
-// convert the numbers from 0 to 9 into smaller factorial divisions. 
-// 4! =4*3! =2!*2!*3!  ,  6! =5!*2!  ,   8! =8*7! =7!*2!*2!*2!*2!   ,  9! =9*8*7! =7!*3!*3!*2! 
+// convert the numbers from 0 to 9 into smaller factorial divisions.
+// 4! =4*3! =2!*2!*3!  ,  6! =5!*2!  ,   8! =8*7! =7!*2!*2!*2!*2!   ,  9! =9*8*7! =7!*3!*3!*2!
 // add them accordingly and sort and reverse at last to get the max num.
 
 int main()
 {
-    string str,ans;
+    LL n;
+    string str,an;
 
-    REP(i,SZ(str)) ans+=con[str[i]-'0'];
+    cin>>n>>str;
 
-    sort(ALL(ans));
-    reverse(ALL(ans));
+    REP(i,SZ(str)) an+=con[str[i]-'0'];
 
-    cout << ans << endl;
+    sort(ALL(an));
+    reverse(ALL(an));
+
+    cout << an << endl;
 
     return 0;
 }
