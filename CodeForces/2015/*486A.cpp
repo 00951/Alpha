23eab6e59877814700aeaf240695e79992092ec5
Mike Meroslov <aarif.shuvo@gmail.com>
2016-01-21 01:40:29 +0600

    Create *486A.cpp

---
+++ CodeForces/2015/*486A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long LL;
+
+
+int main()
+{
+    LL n;
+
+    while(cin >> n) {
+        if(n%2 == 0)
+            cout << n/2 << endl;
+        else
+            cout << n/2 - n << endl;        // pattern .. 1 => -1, 3 => -2, 5 => -3, 7 => -4
+    }                                       // (n/2-n)  ..or..  -(n/2+1)
+
+
+    return 0;
+}
+
+
+
+
+
+//  or.......
+
+
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    long long n, ans, x;
+
+    while(scanf("%I64d", &n)==1)
+    {
+        if(n%2==0)
+        {
+            n=n/2;
+            ans=n*n+n-n*n;
+
+        }
+        else
+        {
+            n/=2;
+            x=n+1;
+
+            ans=n*n+n-x*x;
+
+        }
+        
+        printf("%I64d\n", ans);
+    }
+    return 0;
+}
