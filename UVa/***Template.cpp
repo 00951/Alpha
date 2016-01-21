a6c54a754396e3a8f120109c2da118e323eac56a
xor555 <aarif.shuvo@gmail.com>
2016-01-22 02:18:13 +0600

    Update ***Template.cpp

--- UVa/***Template.cpp
+++ UVa/***Template.cpp
@@ -1,4 +1,60 @@
 #include <bits/stdc++.h>
+#define LL  long long
+#define PII pair<int,int>
+#define VI  vector<int>
+#define VVI vector<vi>
+#define VLL vector<LL>
+#define VS vector<string>
+#define MSI map<string,int>
+#define MII map<int,int>
+#define PII pair<int,int>
+#define PLL pair<LL,LL>
+#define VPI vector<PII>
+#define SZ(x) ((int)(x).size())
+#define ALL(x) (x).begin(),(x).end()
+#define REP(i,n) for(int i=0;i<n;++i)
+#define REPP(i,a,b) for(int i=a;i<=b;++i)
+#define REV(i,b,a) for(int i=b;i>=a;--i)
+#define MP make_pair
+#define PB push_back
+#define ndl puts("");
+#define RI(x) scanf("%lld",&(x))
+#define RII(x,y) scanf("%lld%lld",&(x),&(y))
+#define RIII(x,y,z) scanf("%lld%lld%lld",&(x),&(y),&(z))
+#define DRI(x) LL (x); scanf("%lld",&x)
+#define DRII(x,y) LL x,y; scanf("%lld%lld",&x,&y)
+#define DRIII(x,y,z) LL x,y,z; scanf("%lld%lld%lld",&x,&y,&z)
+#define CLR(a)  memset(a,0,sizeof(a))
+#define SET(a)  memset(a,-1,sizeof(a))
+#define pri(a)     cout<<a<<endl
+#define prii(a,b)   cout<<a<<" "<<b<<endl
+#define priii(a,b,c) cout<<a<<" "<<b<<" "<<c<<endl
+#define priiii(a,b,c,d) cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl
+#define READ(fn) freopen(fn, "r", stdin);
+#define WRITE(fn) freopen(fn, "w", stdout);
+#define __cin ios_base::sync_with_stdio(0); cin.tie(0)
+#define hi puts("Hello World!")
+#define pcs printf("Case %d: ",++cs);
+using namespace std;
+
+
+/*0123456789
+---*/ int cs  = 0;
+const int INF = 1<<29;
+const int eps = 1e-9;
+const int MOD = 1e9+7;
+const int MX  = 1e5+10;
+
+int main(){
+    __cin;
+    
+    return 0;
+}
+// -- jan '16
+
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long            ll
@@ -13,8 +69,8 @@ typedef double               dl
 
 //~ Input
 #define sci(a) scanf("%d", &a);
-#define sci(a,b) scanf("%d%d", &a, &b);
-#define sci(a,b,c) scanf("%d%d%d", &a, &b, &c);
+#define scii(a,b) scanf("%d%d", &a, &b);
+#define sciii(a,b,c) scanf("%d%d%d", &a, &b, &c);
 #define scl(a) scanf("%lld", &a);
 #define scll(a,b) scanf("%lld%lld", &a, &b);
 #define sclll(a,b,c) scanf("%lld%lld%lld", &a, &b, &c);
